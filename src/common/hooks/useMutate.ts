4858f2b7386cd1723d5dba52775feb005039f92b
criztiandev <criztiandev@gmail.com>
2024-09-01 01:50:52 +0800

    Added Hooks and Interceptor

--- src/common/hooks/useMutate.ts
+++ src/common/hooks/useMutate.ts
@@ -1,5 +1,32 @@
-const useMutate = () => {
-  return {};
+import { AxiosError } from "axios";
+
+import { MutationOptions, QueryKey, useMutation } from "@tanstack/react-query";
+
+interface ErrorResponse {
+  error: string;
+  status: number;
+}
+
+interface Props extends MutationOptions {
+  queryKey: QueryKey;
+}
+
+const useMutate = (options: Props) => {
+  return useMutation({
+    ...options,
+    onError: (error: AxiosError) => {
+      if (error instanceof AxiosError && error.response) {
+        const { error: errorMessage } = error.response?.data as ErrorResponse;
+        throw new Error(errorMessage);
+      } else if (error?.request) {
+        throw new Error(
+          error.message || "No response received from the server"
+        );
+      } else {
+        throw new Error("An Unknown error occurred");
+      }
+    },
+  });
 };
 
 export default useMutate;
