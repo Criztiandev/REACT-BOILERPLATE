1be884a71efd49b0a4e0f08338347217e9aadefa
criztian <criztiandev@gmail.com>
2024-10-23 04:07:25 +0800

    ci(env): improve typescript for env file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_DEV_ENVIRONMENT: string;
   readonly VITE_DEV_BASE_URI: string;
+  readonly VITE_DEV_TEST_URI: string;
   readonly VITE_PRODUCTION_BASE_URI: string;
 }
 
