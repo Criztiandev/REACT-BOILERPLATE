95135bc8d79f7964d15f958fbdecc8a9639b81c3
criztian <criztiandev@gmail.com>
2024-10-23 04:27:23 +0800

    refactor: added auto redirect to root when the path is wrong

--- src/feature/admin/admin.routes.tsx
+++ src/feature/admin/admin.routes.tsx
@@ -1,7 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import AdminDashboard from "./page/dashboard";
 
 const adminRoutes = createBrowserRouter([
+  { path: "*", element: <Navigate to="/" replace /> },
   { path: "/", element: <AdminDashboard /> },
 ]);
 
