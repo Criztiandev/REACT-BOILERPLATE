8d00e8385fa0f7d8efa848d9a11c12af810a53a7
criztian <criztiandev@gmail.com>
2024-10-23 04:09:18 +0800

    feat: imrprove persistent routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { RouterProvider } from "react-router-dom";
 import authRoutes from "@/feature/auth/routes";
+import useAuthStore from "./feature/auth/store/auth.store";
+import { IAccountSchema } from "./feature/shared/account/account.interface";
+import userRoutes from "./feature/user/user.routes";
+import adminRoutes from "./feature/admin/admin.routes";
+
+const roleMap: Record<IAccountSchema["role"], any> = {
+  user: userRoutes,
+  admin: adminRoutes,
+};
 
 const App = () => {
-  return <RouterProvider router={authRoutes} />;
+  const { credentials } = useAuthStore();
+
+  const _preferedRoute = credentials ? roleMap[credentials.role] : authRoutes;
+
+  return <RouterProvider router={_preferedRoute} />;
 };
 
 export default App;
