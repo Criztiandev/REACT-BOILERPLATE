b2ccbe21b532d6bcf7f314f275c175a055b7b698
criztian <criztiandev@gmail.com>
2024-10-23 04:07:44 +0800

    refactor: changes the proper way to import env file

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.VITE_DEV_ENVIRONMENT === "test"
-      ? process.env.VITE_DEV_TEST_URI
-      : process.env.VITE_DEV_BASE_URI,
+    import.meta.env.VITE_DEV_ENVIRONMENT === "test"
+      ? import.meta.env.VITE_DEV_TEST_URI
+      : import.meta.env.VITE_DEV_BASE_URI,
   headers: {
     "Content-Type": "application/json",
   },
