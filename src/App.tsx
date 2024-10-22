8accdc32dd9319055445dba4aa6496acd0eefc12
criztian <criztiandev@gmail.com>
2024-10-23 04:26:58 +0800

    refactor: added set credentials for hanlding roles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { RouterProvider } from "react-router-dom";
-import authRoutes from "@/feature/auth/routes";
+import authRoutes from "@/feature/auth/auth.routes";
 import useAuthStore from "./feature/auth/store/auth.store";
 import { IAccountSchema } from "./feature/shared/account/account.interface";
 import userRoutes from "./feature/user/user.routes";
 import adminRoutes from "./feature/admin/admin.routes";
+import { useEffect } from "react";
 
 const roleMap: Record<IAccountSchema["role"], any> = {
   user: userRoutes,
@@ -11,7 +12,11 @@ const roleMap: Record<IAccountSchema["role"], any> = {
 };
 
 const App = () => {
-  const { credentials } = useAuthStore();
+  const { credentials, setCredentials } = useAuthStore();
+
+  useEffect(() => {
+    setCredentials({ UID: "1", role: "admin", token: "123123" });
+  }, []);
 
   const _preferedRoute = credentials ? roleMap[credentials.role] : authRoutes;
 
